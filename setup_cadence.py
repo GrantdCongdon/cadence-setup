03502f95ad61ab53778d8ae1390addd93c43e85a
Grant Congdon <grantdcongdon@gmail.com>
2025-10-16 16:24:14 -0400

    Testing change

--- setup_cadence.py
+++ setup_cadence.py
@@ -1,8 +1,8 @@
 from gdown import download
 from zipfile import ZipFile
-from os import mkdir, chdir, remove, path, system, getenv
+from os import mkdir, chdir, remove, path, system
 from sys import argv, exit
-from shutil import rmtree
+from shutil import rmtree, copyfile
 import tarfile
 
 def main():
@@ -11,13 +11,15 @@ def main():
         print("Usage: python setup_cadence.py <google_drive_link>")
         exit(1)
 
-    download(link, "MSRF_General_Purpose_Plus.zip", fuzzy=True)
+    home_dir = path.expanduser("~")
+
+    #download(link, "MSRF_General_Purpose_Plus.zip", fuzzy=True)
+    copyfile(f"{home_dir}/Downloads/MSRF_General_Purpose_Plus.zip", ".")
     with ZipFile("MSRF_General_Purpose_Plus.zip", 'r') as zip_ref: zip_ref.extractall(".")
 
     mkdir("temp")
     with tarfile.open("MSRF_General_Purpose_Plus/PDK/Cadence OA/tn65cmsp018k3_1_0c/PDK_CRN65GP_v1.0c_official_IC61_20101010_all.tar.gz", 'r') as tar_ref: tar_ref.extractall("./temp/", filter='data')
 
-    home_dir = path.expanduser("~")
     mkdir(f"{home_dir}/Documents/ASIC")
     mkdir(f"{home_dir}/Documents/ASIC/TSMC-65nm")
     with tarfile.open("temp/PDK_CRN65GP_v1.0c_official_IC61_20101010.tar.gz", 'r') as tar_ref: tar_ref.extractall(f"{home_dir}/Documents/ASIC/TSMC-65nm/", filter='data')
