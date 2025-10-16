292ad72e52618dc9d0df501194b150f8d9f82f33
Grant Congdon <grantdcongdon@gmail.com>
2025-10-16 14:32:47 -0400

    Progress

--- setup_cadence.py
+++ setup_cadence.py
@@ -13,12 +13,19 @@ def main():
     
     download(link, "MSRF_General_Purpose_Plus.zip", fuzzy=True)
     with ZipFile("MSRF_General_Purpose_Plus.zip", 'r') as zip_ref: zip_ref.extractall(".")
-    os.remove("MSRF_General_Purpose_Plus.zip")
 
-    with tarfile.open("MSRF_General_Purpose_Plus/PDK/Cadence OA/tn65cmsp018k3_1_0c/PDK_CRN65GP_v1.0c_official_IC61_20101010_all.tar.gz", 'r') as tar_ref: tar_ref.extractall(".")
+    os.mkdir("temp")
+    with tarfile.open("MSRF_General_Purpose_Plus/PDK/Cadence OA/tn65cmsp018k3_1_0c/PDK_CRN65GP_v1.0c_official_IC61_20101010_all.tar.gz", 'r') as tar_ref: tar_ref.extractall("./temp/")
 
     os.mkdir("~/Documents/ASIC/TSMC-65nm")
-    with tarfile.open("PDK_CRN65GP_v1.0c_official_IC61_20101010.tar.gz", 'r') as tar_ref: tar_ref.extractall("~/Documents/ASIC/TSMC-65nm/")
+    with tarfile.open("temp/PDK_CRN65GP_v1.0c_official_IC61_20101010.tar.gz", 'r') as tar_ref: tar_ref.extractall("~/Documents/ASIC/TSMC-65nm/")
 
+    os.remove("temp", recursive=True)
+    os.remove("MSRF_General_Purpose_Plus", recursive=True)
+    os.remove("MSRF_General_Purpose_Plus.zip")
+
+    os.chdir("~/Documents/ASIC/TSMC-65nm/")
+    # execute a pearl script but include injection of the numbers "3," "1," "3," "2" but when prompted for input
+    os.system("./pdkInstall.pl <<< $'3\n1\n3\n2\n'")
 if __name__ == "__main__":
     main()
