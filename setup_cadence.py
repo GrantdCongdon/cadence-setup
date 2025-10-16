acdbdbeeb2397d7c1a9bee1f0a36b3efe6992eac
Grant Congdon <grantdcongdon@gmail.com>
2025-10-16 15:51:38 -0400

    Install repo

--- setup_cadence.py
+++ setup_cadence.py
@@ -28,8 +28,36 @@ def main():
     os.remove("MSRF_General_Purpose_Plus.zip")
 
     os.chdir(f"{home_dir}/Documents/ASIC/TSMC-65nm/")
-    # execute a pearl script but include injection of the numbers "3," "1," "3," "2" but when prompted for input
-    os.system("./pdkInstall.pl <<< $'3\n1\n3\n2\n'")
+    os.system("./pdkInstall.pl <<< $'3\n1\n3\n2\ny\n'")
+
+    with open("lib.defs", "w") as f:
+        f.write(f"DEFINE tsmcN65 {home_dir}/Documents/ASIC/TSMC-65nm/tsmcN65\n")
+        f.write("ASSIGN tsmcN65 libMode shared\n")
+
+    os.chdir(home_dir)
+    with open("cds.lib", "w") as f:
+        f.write("SOFTINCLUDE /package/eda/cadence/IC231.060/share/cdssetup/cds.lib\n")
+        f.write(f"INCLUDE {home_dir}/Documents/ASIC/TSMC-65nm/lib.defs\n")
+
+    os.system("module load cadence/virtuoso")
+    os.system("module load cadence/spectre")
+    os.system("module load keysight/ads")
+    os.system("module load cadence/virtuoso-advanced")
+    os.system("module save ASIC")
+
+    with open(".setup_cadence.sh", "w") as f:
+        f.write("#!/bin/bash\n")
+        f.write("\nmodule restore ASIC\n")
+        f.write("\nexport OA_HOME=/package/eda/cadence/IC231.060/oa_v22.61.013\n")
+        f.write(f"\nexport CDS_LIB_PATH={home_dir}/Documents/ASIC/TSMC-65nm/libs.defs\n")
+        f.write("\nexport PATH=/package/eda/cadence/IC231.060/tools/dfII/bin:$PATH\n")
+
+    os.system("chmod +x .setup_cadence.sh")
+    os.system("source .setup_cadence.sh")
+
+    shutil.rmtree("cadence-setup")
+
+    return
 
 if __name__ == "__main__":
     main()
